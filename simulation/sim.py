d6a18e726d0c8965ee79e275cbdbb789f9e8544c
IanZhang1990 <ian.zhang1990@gmail.com>
2016-05-19 17:13:15 -0400

    cross bar done

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,28 +1,27 @@
 import sys,os
 import bpy
 import mathutils
-from math import cos, sin
+from math import cos, sin, pi
 from mathutils import Vector
 
 class Piece:
     def __init__(self, vertices, normal):
         '''Convex, vertices are in 3D.'''
-        self.verts = vertices;
+        self.verts = [] + vertices;
         self.normal = normal;
 
         for vert in vertices:
             self.verts.append( (vert[0] - normal[0] * 0.4, vert[1] - normal[1] * 0.4, vert[2] - normal[2] * 0.4) );
-
+        
         self.faces = [];
 
-        faces = [];
-        faces.append( tuple([i for i in range(len(vertices))]) );
-        faces.append( tuple([i for i in range(len(self.verts)-1, len(vertices)-1, -1)]) );
+        self.faces.append( tuple([i for i in range(len(vertices))]) );
+        self.faces.append( tuple([i for i in range(len(self.verts)-1, len(vertices)-1, -1)]) );
 
         for i in range(len(vertices)-1):
-            faces.append( (i, i+len(vertices), i+len(vertices)+1, i+1) )
+            self.faces.append( (i, i+len(vertices), i+len(vertices)+1, i+1) )
 
-        faces.append( (len(vertices)-1, len(vertices)*2-1 , 0 + len(vertices), 0 ) )
+        self.faces.append( (len(vertices)-1, len(vertices)*2-1 , 0 + len(vertices), 0 ) )
 
     def mesh_data(self):
         return self.verts, self.faces;
@@ -42,31 +41,39 @@ class CrossBar:
     def mesh_data(self):
         '''Get mesh data including vertices and faces'''
 
-        def get_point(phi_, theta_):
-            return (self.radius * cos(phi_) * cos(theta_), self.radius * cos(phi_) * sin(theta_), self.radius * sin(phi_))
+        def get_point(phi_, theta_, r):
+            return (r * cos(phi_) * cos(theta_), r * cos(phi_) * sin(theta_), r * sin(phi_))
 
         def get_four_vertices(phi_, theta_):
-            eps = 0.01;
-            vert1 = get_point(phi_ - eps,  theta_ - eps)
-            vert2 = get_point(phi_ - eps,  theta_ + eps)
-            vert3 = get_point(phi_ + eps,  theta_ + eps)
-            vert4 = get_point(phi_ + eps,  theta_ - eps)
-            return [vert1, vert2, vert3, vert4];
+            eps = 0.005
+            r= self.radius;
+            width = self.radius * 0.001
+            if phi_ < pi/2.0:
+                vert1 = get_point(phi_,  theta_ - eps, r-width)
+                vert2 = get_point(phi_,  theta_ + eps, r-width)
+                vert3 = get_point(phi_,  theta_ + eps, r+width)
+                vert4 = get_point(phi_,  theta_ - eps, r+width)
+                return [vert1, vert2, vert3, vert4];
+            else:
+                print("what?")
+                vert1 = get_point(phi_,  theta_ + eps, r-width)
+                vert2 = get_point(phi_,  theta_ - eps, r-width)
+                vert3 = get_point(phi_,  theta_ - eps, r+width)
+                vert4 = get_point(phi_,  theta_ + eps, r+width)
+                return [vert1, vert2, vert3, vert4];
 
-        vertice = [];
-        for i in range(0,90, 2):
-            vertice += get_four_vertices( self.phi + i * 2.0, self.theta );
+        vertices = [];
+        for i in range(0,90):
+            ratio = pi/180.0
+            vertices += get_four_vertices( self.phi + i*ratio, self.theta );
 
         faces = [( 0, 1, 2, 3 )];
-        for i in range(0, len(vertices)-1, 4):
+        for i in range(0, len(vertices)-4, 4):
             faces.append( (i+0, i+3, i+7, i+4) );
             faces.append( (i+3, i+2, i+6, i+7) );
             faces.append( (i+2, i+1, i+5, i+6) );
             faces.append( (i+1, i+0, i+4, i+5) );
         faces.append( (len(vertices)-1,len(vertices)-2,len(vertices)-3,len(vertices)-4 ) )
-
-        vertices = [];
-        faces = [];
 
         return vertices, faces;
 
@@ -91,10 +98,14 @@ def create_mesh(name, origin, verts, faces):
 
 def main():
     origin = Vector((0,0,0))
-    vertices = [(0,0,0), (10,0,0), (10,10,0), (0,10,0)]
-    piece = Piece(vertices, (0,0,1));
-    verts, faces = piece.mesh_data();
-    create_mesh( "test", origin, verts, faces )
+    #vertices = [(0,0,0), (10,0,0), (10,10,0), (0,10,0)]
+    #piece = Piece(vertices, (0,0,1));
+    #verts, faces = piece.mesh_data();
+    #create_mesh( "test", origin, verts, faces )
+    
+    bar = CrossBar( (0,0,0), 10, pi/4, 0.3, 0.0 )
+    verts, faces = bar.mesh_data();
+    create_mesh( "bar", origin, verts, faces )
     pass;
 
 main();
